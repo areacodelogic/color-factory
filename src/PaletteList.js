4b7f718eb542bde8a52b92c4970c3170c43ea847
Timothy Egart <areacodelogic@gmail.com>
2020-05-04 15:50:24 -0700

    start styling minipalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
+import MiniPalette from './MiniPalette';
 import {Link} from 'react-router-dom';
 
 class PaletteList extends Component {
   render() {
 		const { palettes } = this.props;
-		console.log(palettes)
     return (
-      <div className='PaletteList'>
+			<div className='PaletteList'>
         <h1>React Colors</h1>
         {palettes.map((palette) => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
+         <MiniPalette {...palette} />
         ))}
       </div>
     );
