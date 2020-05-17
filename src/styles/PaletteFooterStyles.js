f578e6c12fa0b695e30cdb8151e44ffcf5d97a74
Timothy Egart <areacodelogic@gmail.com>
2020-05-16 20:29:38 -0700

    move out styles

---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,14 @@
+export default {
+  PaletteFooter: {
+    backgroundColor: "white",
+    height: "5vh",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    fontWeight: "bold",
+  },
+  emoji: {
+    fontSize: "2.5rem",
+    margin: "0 1rem",
+  },
+};
