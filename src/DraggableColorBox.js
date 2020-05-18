cc94472a910901f457468014836689625c33f79f
Timothy Egart <areacodelogic@gmail.com>
2020-05-17 19:31:42 -0700

    Implement Drag and Drop

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,6 +1,8 @@
 import React from "react";
+import {SortableElement} from 'react-sortable-hoc'
 import { withStyles } from "@material-ui/styles";
 import DeleteIcon from '@material-ui/icons/Delete';
+
 
 const styles = {
   root: {
@@ -34,7 +36,7 @@ const styles = {
       transition: "all 0.3s ease-in-out"
   },
 };
-function DraggableColorBox(props) {
+const DraggableColorBox = SortableElement((props) => {
   const {classes, handleClick, name, color} = props
   return (
     <div
@@ -47,6 +49,6 @@ function DraggableColorBox(props) {
       </div>
     </div>
   );
-}
+})
 
 export default withStyles(styles)(DraggableColorBox);
