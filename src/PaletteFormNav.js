165b55c3feb75e80874b63417c17fddd4be1b8cc
Timothy Egart <areacodelogic@gmail.com>
2020-05-19 17:26:37 -0700

    Fix fzorm Dialog and install emoji-picker

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -59,10 +59,15 @@ class PaletteFormNav extends Component {
       formShowing: false,
     };
     this.showForm = this.showForm.bind(this);
+    this.hideForm = this.hideForm.bind(this);
   }
 
   showForm() {
     this.setState({ formShowing: true });
+  }
+
+  hideForm() {
+    this.setState({formShowing: false})
   }
 
   render() {
@@ -108,7 +113,7 @@ class PaletteFormNav extends Component {
           </div>
         </AppBar>
         {this.state.formShowing && (
-          <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} />
+          <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} hideForm={this.hideForm}/>
         )}
       </div>
     );
--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -7,6 +7,8 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import 'emoji-mart/css/emoji-mart.css'
+import {Picker} from 'emoji-mart';
 
 class PaletteMetaForm extends Component {
   constructor(props) {
@@ -42,19 +44,20 @@ class PaletteMetaForm extends Component {
 
   render() {
     const { newPaletteName } = this.state;
-
+    const { hideForm, handleSubmit } = this.props;
     return (
       <Dialog
         open={this.state.open}
-        onClose={this.handleClose}
+        onClose={hideForm}
         aria-labelledby='form-dialog-title'>
         <DialogTitle id='form-dialog-title'>Choose a Palette Name</DialogTitle>
-        <ValidatorForm onSubmit={() => this.props.handleSubmit(newPaletteName)}>
+        <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
           <DialogContent>
             <DialogContentText>
               Please enter a name for your new palette. Make sure the name is
               unique.
             </DialogContentText>
+            <Picker />
 
             <TextValidator
               label='Palette Name'
@@ -68,7 +71,7 @@ class PaletteMetaForm extends Component {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={this.handleClose} color='primary'>
+            <Button onClick={hideForm} color='primary'>
               Cancel
             </Button>
             <Button variant='contained' color='primary' type='submit'>
