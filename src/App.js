c7ff37262fcf6538e47fea6866c14c0f8719c404
Timothy Egart <areacodelogic@gmail.com>
2020-05-19 20:27:00 -0700

    Save palettes to localstorage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { generatePalette } from "./colorhelpers";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
   }
@@ -21,9 +22,17 @@ class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
   }
-
+  syncLocalStorage() {
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
+  }
   render() {
     return (
       <Switch>
